a090225659d1bf78db76c6cb98705583ca749072
Datta6788 <ravuridattasai88@gmail.com>
2026-05-26 13:12:54 +0530

    posters

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import requests
 from scipy.sparse import load_npz
+from rapidfuzz import process 
 
 knn_model = joblib.load(r"D:\PRJ1\PROJECT1\models\knn_model.pkl")
 sparse_matrix = load_npz(r"D:\PRJ1\PROJECT1\models\sparse_matrix.npz")
@@ -14,20 +16,46 @@ def knn_recc(ani_name, no_recc=10):
     ani_index = ani_to_index[ani_name]
     distance, index = knn_model.kneighbors(sparse_matrix[ani_index],n_neighbors=no_recc + 1)
     recco = []
-
     for i in range(1, len(distance.flatten())):
         recommended_index = int(index.flatten()[i])
         recommended_anime = index_to_ani[recommended_index]
         distance_value = float(distance.flatten()[i])
-        recco.append({"Anime": recommended_anime,"Distance": round(distance_value,4)})
-    return recco
+        recco.append({"Anime": recommended_anime})
+    return pd.DataFrame(recco)
 
+def fuzzy(input,anime_list):
+    match=process.extractOne(input,anime_list)
+    if match is None:
+        return None
+    return match[0]
+
+def posters(ani_name):
+    url=(f"https://api.jikan.moe/v4/anime?q={ani_name}&limit=1")
+    response=requests.get(url)
+    data=response.json()
+    try:
+        url=data["data"][0]["images"]["jpg"]["image_url"]
+        return url
+    except:
+        return None
+
+ani_list=list(ani_to_index.keys())
 st.title("Anime RECOMMENDATION SYSTEM")
 ani_input = st.text_input("Enter anime name")
 top_n = st.selectbox("No.of recommendations",[5, 10, 25, 50, 100])
 if st.button("Recommend"):
-    result = knn_recc(ani_input,top_n)
+    matched_ani=fuzzy(ani_input,ani_list)
+    result = knn_recc(matched_ani,top_n)
     if result is None:
         st.error("Anime not found!")
     else:
-        st.dataframe(result,use_container_width=True)
+        st.success(f"Showing results for: {matched_ani}")
+        for _,row in result.iterrows():
+            url=posters(row["Anime"])
+            Pcolumn,Acolumn=st.columns([1,3])
+            with Pcolumn:
+                if url:
+                    st.image(url,width=150)
+            with Acolumn:
+                st.subheader(row["Anime"])
+            st.markdown("---")
